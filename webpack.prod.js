408e506ee8d6de0c088571297950e36f184cc6c7
Alec Gregory <alec_g@beezwax.net>
2021-03-17 11:53:43 -0700

    Add targets

--- webpack.prod.js
+++ webpack.prod.js
@@ -5,5 +5,6 @@ const InlineChunkHtmlPlugin = require("react-dev-utils/InlineChunkHtmlPlugin");
 
 module.exports = merge(common, {
   mode: "production",
+  target: ["web", "es5"],
   plugins: [new InlineChunkHtmlPlugin(HtmlWebpackPlugin, [/app.bundle/])],
 });
