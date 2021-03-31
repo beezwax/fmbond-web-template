0bf1985249e473f31fea085b44f7e9cc17fc4973
Alec Gregory <alec_g@beezwax.net>
2021-03-31 11:45:30 -0700

    Remove dependencies and add browserslistrc

--- webpack.prod.js
+++ webpack.prod.js
@@ -5,6 +5,5 @@ const InlineChunkHtmlPlugin = require("react-dev-utils/InlineChunkHtmlPlugin");
 
 module.exports = merge(common, {
   mode: "production",
-  target: ["web", "es5"],
   plugins: [new InlineChunkHtmlPlugin(HtmlWebpackPlugin, [/app.bundle/])],
 });
