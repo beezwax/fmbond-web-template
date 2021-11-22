834c0e719e3becd8a672d4ffe83a4c7c03611e6a
Alec Gregory <alec_g@beezwax.net>
2021-11-22 08:54:38 -0800

    Remove react-dev-utils dependency

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,7 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const InlineChunkHtmlPlugin = require("react-dev-utils/InlineChunkHtmlPlugin");
+const InlineChunkHtmlPlugin = require('./plugins/inline-chunk-html-plugin')
 
 module.exports = merge(common, {
   mode: "production",
