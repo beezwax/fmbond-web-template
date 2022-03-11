6d7144a77617f6395abefa8d7b91b536fa8966bf
Alec Gregory <alec_g@beezwax.net>
2022-03-10 18:32:21 -0800

    Add installation script for fmbond-filemaker-suite file

---
+++ install_fm_suite.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+
+const installSuite = (sourcePath, destPath, overwrite) => {
+  // check file exists
+  if (!overwrite) {
+    if(fs.existsSync(destPath)) {
+      throw new Error("File exists and overwrite flag is no set");
+    }
+  }
+  fs.copyFileSync(sourcePath, destPath);
+}
+
+// get command line args
+const args = process.argv.slice(2);
+
+// first arg is destination path
+const destPath = args[0] ? args[0] : "./src/filemaker/fmbond-filemaker-suite.fmp12";
+
+// second arg is overwrite flag
+const overwrite = !!args[1];
+
+// source path is
+const sourcePath = "./node_modules/@beezwax/fmbond-filemaker-suite/fmbond-filemaker-suite.fmp12"
+
+try {
+  installSuite(sourcePath, destPath, overwrite);
+  console.log("fmbond-filemaker-suite successfully installed!");
+} catch (error) {
+  console.log(error);
+}
+
