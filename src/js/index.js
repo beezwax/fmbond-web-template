4094217599c1c81775adc325a1e7c2a631672ba4
Alec Gregory <alec_g@beezwax.net>
2022-03-23 06:05:06 -0700

    Update default app content

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,33 @@
 import FMBond from "@beezwax/fmbond";
 import "../scss/app.scss";
 
-const para = document.createElement("p");
-para.innerText = "FMBond refers to a " + typeof FMBond;
-document.body.appendChild(para);
+const main = document.createElement("main");
+
+const heading = document.createElement("h1");
+heading.innerHTML = "FMBond";
+
+const subheading = document.createElement("h2");
+subheading.innerText = typeof FMBond === "function" ? "is working for you" : "is having issues";;
+
+const logo = document.createElement("div");
+logo.classList.add("logo");
+logo.innerText = "∞";
+
+const instruction = document.createElement("p");
+instruction.innerText = "Edit and save ./src/js/index.js to update this page.";
+
+const learn = document.createElement("p");
+const learnLink = document.createElement("a");
+learnLink.setAttribute("href", "https://github.com/beezwax/fmbond-js");
+learnLink.setAttribute("target", "_blank");
+learnLink.innerText = `Learn FMBond`;
+learn.appendChild(learnLink);
+
+main.appendChild(heading);
+main.appendChild(subheading);
+main.appendChild(logo);
+if(window.location.hostname === "localhost") {
+  main.appendChild(instruction);
+}
+main.appendChild(learn);
+document.body.appendChild(main);
