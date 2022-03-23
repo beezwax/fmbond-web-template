fdba44c750a3a493f23d800758373a8f86ea1151
Alec Gregory <alec_g@beezwax.net>
2022-03-23 10:19:49 -0700

    Add custom header in devServer

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,5 +1,6 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
+const packageInfo = require("./package.json");
 
 module.exports = merge(common, {
   mode: "development",
@@ -8,4 +9,12 @@ module.exports = merge(common, {
   entry: {
     entry: ["whatwg-fetch", "core-js/features/promise", "./src/js/index.js"],
   },
+  devServer: {
+    headers: [
+      {
+        key: "packageName",
+        value: packageInfo.name
+      }
+    ]
+  }
 });
