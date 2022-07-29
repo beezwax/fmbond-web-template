a6c29ef74c9d3da7194f792e206c69d4e5ba44a4
Alec Gregory <alec_g@beezwax.net>
2022-07-29 10:51:14 -0700

    Debug cli

--- bin/cli.js
+++ bin/cli.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+const {exec, execSync} = require("child_process");
+execSync("npm install");
 const CLI = require('../lib/cli/index.js');
 const cli = new CLI();
 cli.start(process.argv.slice(2));
